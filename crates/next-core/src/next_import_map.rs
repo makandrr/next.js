91f664e041e1220219bacf8b14786026c84b0523
Sam Selikoff <sam.selikoff@gmail.com>
2025-10-03 15:55:25 -0400

    wip

--- crates/next-core/src/next_import_map.rs
+++ crates/next-core/src/next_import_map.rs
@@ -989,6 +989,7 @@ async fn apply_vendored_react_aliases_server(
         // This is used in the server runtime to import React Server Components.
         alias.extend(fxindexmap! {
             rcstr!("next/navigation") => rcstr!("next/dist/api/navigation.react-server"),
+            rcstr!("next/link") => rcstr!("next/dist/api/link.react-server"),
         });
     }
 
@@ -1018,6 +1019,7 @@ async fn rsc_aliases(
         // This is used in the server runtime to import React Server Components.
         alias.extend(fxindexmap! {
             rcstr!("next/navigation") => rcstr!("next/dist/api/navigation.react-server"),
+            rcstr!("next/link") => rcstr!("next/dist/api/link.react-server"),
         });
     }
 
--- packages/next/src/build/swc/generated-native.d.ts
+++ packages/next/src/build/swc/generated-native.d.ts
@@ -34,18 +34,18 @@ export declare class ExternalObject<T> {
     [K: symbol]: T
   }
 }
-export interface TransformOutput {
-  code: string
-  map?: string
-  output?: string
-  diagnostics: Array<string>
-}
 export declare function mdxCompile(
   value: string,
   option: Buffer,
   signal?: AbortSignal | undefined | null
 ): Promise<unknown>
 export declare function mdxCompileSync(value: string, option: Buffer): string
+export interface TransformOutput {
+  code: string
+  map?: string
+  output?: string
+  diagnostics: Array<string>
+}
 export declare function minify(
   input: Buffer,
   opts: Buffer,
